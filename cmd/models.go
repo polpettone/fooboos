e3be285551618ba227e3dffc1c4daff2153fec1b
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-12-26 21:04:42 +0100

    fuzzy search for fooboos keywords

--- cmd/models.go
+++ cmd/models.go
@@ -1,8 +1,25 @@
 package cmd
 
+import "github.com/schollz/closestmatch"
 
 type Fooboos struct {
 	Entries map[string][]string
 }
 
+func (fooboos Fooboos) search(query string) []string {
+	var keywords []string
+	for k, _ := range fooboos.Entries {
+		keywords = append(keywords, k)
+	}
 
+	result := fuzzySearch(keywords, query)
+
+	return result
+}
+
+func fuzzySearch(keywords []string, query string) []string {
+	bagSizes := []int{2}
+	cm := closestmatch.New(keywords, bagSizes)
+	result := cm.ClosestN(query, 2)
+	return result
+}
---
+++ cmd/models_test.go
@@ -0,0 +1,100 @@
+package cmd
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_fuzzySearch(t *testing.T) {
+	tests := []struct {
+		name     string
+		keywords []string
+		query    string
+		want     []string
+	}{
+
+		{
+			name:     "0",
+			keywords: []string{"one", "two", "three"},
+			query:    "one",
+			want:     []string{"one"},
+		},
+
+		{
+			name:     "1",
+			keywords: []string{"one", "two", "three"},
+			query:    "on",
+			want:     []string{"one"},
+		},
+
+		{
+			name:     "2",
+			keywords: []string{"one", "on", "three"},
+			query:    "on",
+			want:     []string{"on", "one"},
+		},
+
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fuzzySearch(tt.keywords, tt.query); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fuzzySearch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
+
+func TestFooboos_search(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		fields map[string][]string
+		args   string
+		want   []string
+	}{
+
+		{name: "find exactly",
+			fields: map[string][]string{
+				"one": {"url0", "url1"},
+				"two": {"url0", "url1"},
+			},
+			args: "one",
+			want: []string{"one"}},
+
+		{name: "find fuzzy",
+			fields: map[string][]string{
+				"one": {"url0", "url1"},
+				"two": {"url0", "url1"},
+			},
+			args: "on",
+			want: []string{"one"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fooboos := Fooboos{
+				Entries: tt.fields,
+			}
+			if got := fooboos.search(tt.args); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("search() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFoo(t *testing.T) {
+
+	tests := []struct {
+		name string
+		args string
+	}{
+		{"one", "args"},
+	}
+
+	if len(tests) != 1 {
+		t.Errorf("Wanted 1 got %d", len(tests))
+	}
+
+}
