97da5aeb9a503666a45713d8a907519794da1e69
Stefan Heiden <stefan.heiden@qudosoft.de>
2021-01-01 16:47:13 +0100

    edit a single fooboo

--- cmd/edit.go
+++ cmd/edit.go
@@ -21,15 +21,16 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"gopkg.in/yaml.v2"
+	"strings"
 )
-
 
 func NewEditCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "edit",
-		Short: "edit",
+		Short: "edit <keyword>",
+		Long:  "Open fooboos with given keyword in a text editor. If keyword is missing all fooboos will be open in a text editor.",
 		Run: func(cmd *cobra.Command, args []string) {
-			stdout, err := handleEditCommand()
+			stdout, err := handleEditCommand(args)
 
 			if err != nil {
 				fmt.Println(err)
@@ -40,29 +41,68 @@ func NewEditCmd() *cobra.Command {
 	}
 }
 
-func handleEditCommand() (string, error) {
+func handleEditCommand(args []string) (string, error) {
 	fooboosFile := viper.GetString("path_to_fooboos")
-	content, err := loadRaw(fooboosFile)
 
-	if err != nil {
-		return "", err
+	if len(args) < 1 {
+		content, err := loadRaw(fooboosFile)
+		if err != nil {
+			return "", err
+		}
+
+		result, err := pkg.CaptureInputFromEditor(string(content))
+
+		fooboos := &Fooboos{}
+		err = yaml.Unmarshal([]byte(result), &fooboos)
+		if err != nil {
+			return "", err
+		}
+
+		err = writeRaw(fooboosFile, []byte(result))
+
+		if err != nil {
+			return "", err
+		}
+
+		return "saved", nil
+	} else {
+		keyword := args[0]
+
+		fooboosFile := viper.GetString("path_to_fooboos")
+		fooboos, err := loadFooboos(fooboosFile)
+
+		if err != nil {
+			return "", err
+		}
+
+		urls, found := fooboos.Entries[keyword]
+
+		if !found {
+			return fmt.Sprintf("no bookmark found for keyword %s", keyword), nil
+		}
+
+		content := ""
+		for _, url := range urls {
+			content += url + "\n"
+		}
+
+		rawResult, err := pkg.CaptureInputFromEditor(content)
+		result := strings.Split(rawResult, "\n")
+		fooboos.Entries[keyword] = result
+		out, err := yaml.Marshal(fooboos)
+
+		if err != nil {
+			return "", err
+		}
+
+		err = writeRaw(fooboosFile, out)
+
+		if err != nil {
+			return "", err
+		}
+
+		return "done", nil
 	}
-
-	result, err := pkg.CaptureInputFromEditor(string(content))
-
-	fooboos := &Fooboos{}
-	err = yaml.Unmarshal([]byte(result), &fooboos)
-	if err != nil {
-		return "", err
-	}
-
-	err = writeRaw(fooboosFile, []byte(result))
-
-	if err != nil {
-		return "", err
-	}
-
-	return "saved", nil
 }
 
 func init() {
